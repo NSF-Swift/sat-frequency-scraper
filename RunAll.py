cd71fdd65d77213a685646604352b85da87df866
Stoverbrock1 <ztgstover@berkeley.edu>
2023-03-01 15:43:19 -0800

    Sort Transmitters by name alphabetically

--- RunAll.py
+++ RunAll.py
@@ -1,6 +1,7 @@
 import mechanicalsoup
 import requests
 import pandas as pd
+import numpy as np
 
 ### Import Scrapers
 from RadioGuyScraper import Scraper as rgs
@@ -16,7 +17,34 @@ for each in myDicts:
     stats += each['Status']
     descs += each['Description']
 
+"""
+Sort based off Names
+"""
+
+nameInds = np.argsort(np.array(names))
+names = list(np.array(names)[nameInds])
+IDs = list(np.array(IDs)[nameInds])
+freqs = list(np.array(freqs)[nameInds])
+stats = list(np.array(stats)[nameInds])
+descs = list(np.array(descs)[nameInds])
+
 compDict = {'ID':IDs, 'Name':names, 'Frequency [MHz]':freqs, 'Status':stats, 'Description':descs}
 
+"""
+index = 0
+for each in names:
+    if (freqs[index] in freqs[index + 1:]):
+        print(each)
+    index += 1
+
+"""
+
+
+
+
+
+
+
 myFrame = pd.DataFrame.from_dict(compDict)
+#print(sorted(names))
 myFrame.to_csv('SatList.csv', index=False)
