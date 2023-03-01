7ea404c3263b8ef16c049bf508df1f7d33c97161
Stoverbrock1 <ztgstover@berkeley.edu>
2023-02-28 17:10:29 -0800

    Created Scraper for USRadioGuy website

---
+++ RadioGuyScraper.py
@@ -0,0 +1,41 @@
+import mechanicalsoup
+import requests
+import pandas as pd
+
+def Scraper():
+    """
+    Returns dictionary consisting of ID, Name, Frequency, Status, and Description
+    """
+    url = 'https://usradioguy.com/satellite-frequencies/'
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open(url)
+    myRes = browser.get_current_page().find_all("tr")
+
+    myDict = {'ID':[], 'Name':[], 'Frequency':[], 'Status':[], 'Description':[],}
+
+    for each in myRes[1:]:
+        satName = str(each.contents[1].contents[0])
+        ID = 'None'
+        FreqRaw = str(each.contents[7].contents[0]).strip()
+        mInd = FreqRaw.index("M")
+        Freq = FreqRaw[:mInd].strip()
+        statRaw = str(each.contents[11].contents[0]).strip()
+        if (statRaw == 'D'):
+            stat = 'inactive'
+        else:
+            stat = 'active'
+        Desc = str(each.contents[12].contents[0]).strip()
+        myDict['ID'] = myDict['ID'] + [ID]
+        myDict['Name'] = myDict['Name'] + [satName]
+        myDict['Frequency'] = myDict['Frequency'] + [Freq]
+        myDict['Status'] = myDict['Status'] + [stat]
+        myDict['Description'] = myDict['Description'] + [Desc]
+    return myDict
+
+
+
+
+
+if __name__ == "__main__":
+    myFrame = pd.DataFrame.from_dict(Scraper())
+    print(myFrame)
