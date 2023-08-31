35d6d48707a77561c95a21c32ea5d2f9c39f60dd
Stoverbrock1 <ztgstover@berkeley.edu>
2023-08-31 19:05:05 -0400

    updated runall further

--- SpaceTrackScraper.py
+++ SpaceTrackScraper.py
@@ -12,12 +12,29 @@ def Scraper():
     trunc_df = spacetrack_df[["NORAD_CAT_ID", "SATNAME", "COMMENT", "PERIOD"]]
 
     spacetrack_dict = trunc_df.to_dict("list")
+    periods = np.array([float(x) for x in spacetrack_dict.pop("PERIOD")])
+    mean_motion = (60*24)/periods
+    orbits = []
+
+    for mot in mean_motion:
+        if ((mot >= 0) and (mot < 2)):
+            orbits += ['GEO']
+        elif ((mot >= 2) and (mot <= 15)):
+            orbits += ['MEO']
+        elif (mot >= 15):
+            orbits += ['LEO']
+        else:
+            orbits += ['None']
+    #celestrak_df.loc[(p >= 0) & (p < 2), "ORBIT_TYPE"] = "GEO"
+    #celestrak_df.loc[(p >= 2) & (p < 15), "ORBIT_TYPE"] = "MEO"
+    #celestrak_df.loc[(p >= 15), "ORBIT_TYPE"] = "LEO"
+
 
     myDict = {
             "ID":[str(x) for x in spacetrack_dict.pop("NORAD_CAT_ID")],
             "Name":[str(x) for x in spacetrack_dict.pop("SATNAME")],
             "Description":[str(x) for x in spacetrack_dict.pop("COMMENT")],
-            "Orbit":[str(x) for x in spacetrack_dict.pop("Orbit")]
+            "Orbit":orbits
     }
     myDict["Frequency"] = ["None" for x in myDict["ID"]]
     myDict["Bandwidth/Baud"] = ["None" for x in myDict["ID"]]
